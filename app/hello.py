8d8f90c6b5a425392ce38cff3e3c129aab51e1ab
Steve Scammon <stevescammon@me.com>
2019-11-25 21:00:42 -0500

    Layouts

--- app/hello.py
+++ app/hello.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	user = {'username' : 'Steve'}
-	name = request.args.get("name", "World")
-	return render_template('index.html', title='Home', user=user)
+	posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+	return render_template('index.html', title='Home', user=user, posts=posts)
 
 if __name__ == '__main__':
 	app.run()
