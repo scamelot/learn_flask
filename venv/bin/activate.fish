8a9b3fbd221fc91addf8484ee5f594654d663d33
Scammon <scammons@c02tw0gwhtdh.corp.espn.pvt>
2019-12-03 16:04:00 -0500

    Followers

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/steve/Documents/Python/learn_flask/venv"
+set -gx VIRTUAL_ENV "/Users/scammons/Projects/learn_flask/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
